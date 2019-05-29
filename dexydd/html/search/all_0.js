b26866e07f9db1a1d75a35b3b951c708759471f3
arnisimor <43606398+arnisimor@users.noreply.github.com>
2019-05-29 14:16:35 +0300

    V2.0
    Pridėta dokumentacija su Doxygen.
    Atlikti 4 unit testai

---
+++ dexydd/html/search/all_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['stud',['stud',['../classstud.html',1,'']]]
+];
---
+++ testai.cpp
@@ -0,0 +1,46 @@
+#define CATCH_CONFIG_MAIN
+#include"vector"
+#include"stud.h"
+#include"catch.hpp"
+
+using std::vector;
+using std::string;
+TEST_CASE("Medianos patikrinimas")
+{
+    stud testas;
+    vector<int> nd = {4,5,8,9};
+    testas.set_nd(nd);
+    testas.median();
+
+    REQUIRE(testas.mediana()==6.5);
+}
+
+
+TEST_CASE("Galutinio balo patikrinimas")
+{
+    stud testas;
+    vector<int> nd = {3,8,7,5,9};
+    int egz=5;
+    testas.set_egzaminas(egz);
+    testas.set_nd(nd);
+    testas.galutinis();
+
+    REQUIRE(testas.galb()==5.56);
+}
+
+TEST_CASE("Vardo patikrinimas")
+{
+    stud testas;
+    string vardas="Vardas01";
+    testas.set_vardas(vardas);
+     REQUIRE(testas.vardas()=="Vardas01");
+
+}
+TEST_CASE("Pavardes patikrinimas")
+{
+    stud testas;
+    string pavarde="Pavarde01";
+    testas.set_pavarde(pavarde);
+     REQUIRE(testas.pavarde()=="Pavarde01");
+
+}
